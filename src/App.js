d6f86f3a34b1845bb18c9e43eb931418838c8105
Richard Nolet <richernote@gmail.com>
2023-05-08 17:17:41 -0700

    Made a clock to get practice with useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import './App.css';
 import Heading from "./Heading"
-import ModeToggler from "./ModeToggler"
 
 function App() {
-  const date = new Date()
+  let date = new Date().toLocaleTimeString()
+  const [word, setWord] = React.useState(date)
 
-  const [word, setWord] = React.useState("Eat")
-
-  function handleClick(){
-    setWord("Dine")
-
-  }
+  let timer = (() => {
+    return(
+      setTimeout(() => {
+        date = new Date()
+        setWord(date.toLocaleTimeString())
+      }, 1000)
+    )
+  })()
+ 
+  
 
   return (
     <>
-      <Heading message={word + " at Little Lemon"} />
-      <button onClick={ handleClick }>Click</button>
-      {/* <ModeToggler /> */}
+      <Heading message={word} />
     </>
   )
 }
