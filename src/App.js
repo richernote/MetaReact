ca3cdca3d9e566569aeecc633071e540c49f924e
Richard Nolet <richernote@gmail.com>
2023-05-10 14:12:49 -0700

    Changed App.js to match 'Dynamic Events' exercise.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 
 function App() {
 
-  handleClick = () => {
-    randomNumber = Math().floor(Math.random() * 3) + 1
-    console.log(randomNumber)
-
-
+  function randomNum() {
+    const ranInt = Math.floor(Math.random() * 3) + 1
+    console.log(ranInt)
+    const userInput = prompt("What's you're guess?")
+    alert(`Your guess was ${userInput}. The answer is ${ranInt}.\nYou were ${userInput == ranInt? "correct!" : "wrong, try again"}
+    `)
   }
 
   return (
     <>
       <h1>
-      "Task: Add a button and handle a click event"
+        Task: Add a button and handle a click event
       </h1>
 
-      <button onClick={ handleClick }>
+      <button onClick={ randomNum }>
         Etner a Number between 1 and 3
       </button>
     </>
